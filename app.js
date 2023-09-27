a3590dcae42b315768ab15fa87ecc6ce2cf32f48
root <root@DESKTOP-J195E5S>
2023-09-27 17:49:47 +0800

    impement Shaun mydetails seller

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express from "express";
 import prisma from "./src/utils/prisma.js";
 import cors from "cors";
 import morgan from "morgan";
-import cors from "cors";
 import authSellersRouter from "./src/controllers/auth.Seller.controllers.js"
 import authUsersRouter from "./src/controllers/auth.User.controllers.js"
 import sellersRouter from "./src/controllers/sellers.controllers.js";
@@ -19,6 +18,26 @@ app.use("/users", usersRouter);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+//need to implement auth later on
+app.get("/my-details/:sellerId", async (req, res) => {
+  const sellerId = parseInt(req.params.sellerId); // Parse userId from the URL parameter
+
+  try {
+    // Use Prisma to find images owned by the specified user
+    const myDetails = await prisma.seller.findUnique({
+      where: {
+        id: sellerId,
+      },
+    });
+
+    // Return the mydetails as JSON response
+    return res.json({ myDetails, sellerId }); // added user: for redirect.. place change to userid if needed
+  } catch (error) {
+    // Handle errors and return an error response if needed
+    console.error("Error retrieving details:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
 });
 
 export default app;
