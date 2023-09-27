fda949445b3c72d3190b405bb00a8a508a0b7677
root <root@DESKTOP-J195E5S>
2023-09-27 11:42:06 +0800

    frankie did seller & user login 2

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ import express from "express";
 import prisma from "./src/utils/prisma.js";
 import morgan from "morgan";
 import cors from "cors";
-import sellersRouter from "./src/controllers/auth.Seller.controllers.js"
-import usersRouter from "./src/controllers/auth.User.controllers.js"
+import authSellersRouter from "./src/controllers/auth.Seller.controllers.js"
+import authUsersRouter from "./src/controllers/auth.User.controllers.js"
 
 const app = express();
 app.use(morgan("combined"));
 app.use(cors());
 app.use(express.json());
-app.use("/auth-seller", sellersRouter)
-app.use("/auth-user", usersRouter)
+app.use("/auth-seller", authSellersRouter)
+app.use("/auth-user", authUsersRouter)
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
